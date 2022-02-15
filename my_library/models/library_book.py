106ee3d0fdd2a095dad8f7b6778d38cefa0fd83b
JPLEVYU <juanpablolevy@gmail.com>
2022-02-15 18:33:11 -0300

    [ADD] Editors added

--- my_library/models/library_book.py
+++ my_library/models/library_book.py
@@ -9,6 +9,7 @@ class LibraryBook(models.Model):
     name = fields.Char('Title', required=True)
     date_release = fields.Date('Release Date')
     author_ids = fields.Many2many('res.partner', string='Authors')
+    editor_ids = fields.Many2many('res.partner', string='Editors')
     state = fields.Selection(
         [('draft', 'Not Available'),
          ('available', 'Available'),
